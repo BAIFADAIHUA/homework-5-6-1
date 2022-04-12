2af45fa50904437cd06cd9cc811de57502a0a47a
BAIFADAIHUA <3536043700@qq.com>
2022-04-12 22:45:48 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Student s1 = new Student("白发", 20);
+s1.Show();
+Undergraduate u1 = new Undergraduate("戴花", 30, "本科");
+u1.Show();
+Console.ReadKey();
+//建立一个学生类基类
+class Student
+{
+    public Student (string name,int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public string Name;
+    public int Age;
+    public void Show()
+    {
+        Console.WriteLine("姓名" + Name + "\t年龄" + Age);
+    }
+}
+//建立一个本科生派生类
+class Undergraduate : Student
+{
+    public Undergraduate(string name ,int age,string degree) : base(name,age)
+    {
+        Degree = degree;
+    }
+    public string Degree;
+    public new void Show()
+    {
+        Console.WriteLine("姓名：" + Name + "\t年龄：" + Age +"\t学位："+ Degree);
+    }
+}
